07768a15c76693b3bb38c128e9befa3ea7013acb
laizu akram <laizuakram98@gmail.com>
2023-02-02 11:26:13 +0600

    add signup data api

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 
 
 require('dotenv').config()
-const port = 5000
+const port = process.env.PORT || 5000
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 //middle ware
@@ -15,42 +15,39 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.USER_PASS}@cluster0.xc0o9.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-
-async function run(){
+async function run() {
     try {
-        await client.connect();
+  
+      await client.connect();
+      const banaoCollection = client.db("BanaoUsers").collection("users");
+   
 
-        const usersCollection = client.db("banao-login-data").collection("user-data");
+  app.post('/SignUp', async(req, res) =>{
+    const data = req.body;
+    const result = await banaoCollection.insertOne(data)
 
-        app.post('/login', async(req, res) =>{
-            const query = req.body.password;
-            const password = await usersCollection.findOne({password: query});
-            //console.log(password);
-            if(password){
-                res.send('succesfully login')
-            }
-            res.send('please provide correct password')
-
-
-        });
-
-        //post users data
-        app.post('/user', async (req, res) =>{
-            let userData = req.body;
-            const result = await usersCollection.insertOne();
-            res.send(result);
-          })
+    res.send(result);
+  })
+  
+      
+  
+        
+  
+  
+  
     } finally {
-        //await clien.close();
+      // await client.close();
     }
-}
-run().catch(console.dir);
-
-app.get('/', (req, res) => {
-  res.send('welcome to banao server!')
-})
-
-
-app.listen(port, ()=>{
-    console.log(`app listening on port ${port}`)
-})
+  }
+  run().catch(console.dir);
+  
+  
+  
+  
+  app.get('/', (req, res) => {
+    res.send('welcome to banao server')
+  })
+  
+  app.listen(port, () => {
+    console.log(` banao server listening on port ${port}`)
+  })
